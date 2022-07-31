3076c0c847c1952497934d8d150d987be6520755
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-07-31 18:53:40 -0500

    add axios adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokeResponse } from './interfaces/poke-response.interface';
 
@@ -9,18 +9,18 @@ import { PokeResponse } from './interfaces/poke-response.interface';
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
 
   //* Inject Model
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({}); // delete * from pokemons;
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     // const inserPromisesArray = [];
     const pokemonToInsert: { name: string, no: number; }[] = [];
@@ -38,7 +38,7 @@ export class SeedService {
     });
 
     await this.pokemonModel.insertMany(pokemonToInsert);
-    
+
     // await Promise.all(inserPromisesArray);
 
     return 'Seed Executed';
