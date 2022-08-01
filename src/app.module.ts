839ba459b3e27af10657331602077599ed26e948
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2022-07-31 23:42:48 -0500

    add variables de entorno y load

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path'; // en Node 
 import { CommonModule } from './common/common.module';
+import { EnvConfiguration } from './config/env.config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }), //! VARIABLES DE ENTRNO DE CONFIGURACION
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
     PokemonModule,
     CommonModule,
-    SeedModule
+    SeedModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+  constructor() {
+    console.log(process.env);
+  }
+}
